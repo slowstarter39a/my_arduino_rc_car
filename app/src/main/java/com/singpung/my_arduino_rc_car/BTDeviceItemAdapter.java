e72fac50f5178d604ce757f24460f92c5842f219
slowstarter80 <slowstarter80@daum.net>
2021-04-20 23:51:12 +0900

    Implemented BT Device searching function

---
+++ app/src/main/java/com/singpung/my_arduino_rc_car/BTDeviceItemAdapter.java
@@ -0,0 +1,78 @@
+package com.singpung.my_arduino_rc_car;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.BaseAdapter;
+import android.app.Activity;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+
+import java.util.ArrayList;
+import android.view.LayoutInflater;
+
+public class BTDeviceItemAdapter extends BaseAdapter {
+    private ArrayList<String> items;
+    private Activity activity;
+
+    public BTDeviceItemAdapter(Activity activity) {
+        this.activity = activity;
+        items = new ArrayList<String>();
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    public void addItem(String item) {
+        items.add(item);
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public void clearAll() {
+        items.clear();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ListViewHolder holder = null;
+        TextView name;
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) activity.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            convertView = inflater.inflate(R.layout.dialog_textview_layout, parent, false);
+            name = (TextView) convertView.findViewById(R.id.textView);
+
+            holder = new ListViewHolder();
+            holder.name = name;
+
+            convertView.setTag(holder);
+            name.setVisibility(View.VISIBLE);
+        }
+        else {
+            holder = (ListViewHolder) convertView.getTag();
+            name = holder.name;
+        }
+        name.setText(items.get(position));
+
+        return convertView;
+
+    }
+
+    private class ListViewHolder {
+        TextView name;
+    }
+}
